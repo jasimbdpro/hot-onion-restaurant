8bdb469a249bd31a14f2925adef62041352d22c3
Jasim Uddin <jasimbdpro@gmail.com>
2024-09-24 18:04:55 +0600

    nw

--- src/components/MainNav.jsx
+++ src/components/MainNav.jsx
@@ -11,7 +11,7 @@ const MainNav = () => {
             <Link className='m-0 p-0 text-decoration-none' to='/'><img className='w-25 img-fluid' src="/images/logo2.png" alt="" /></Link>
             <div>
                 <Link className=' text-decoration-none p-1'><FontAwesomeIcon icon={faCartShopping} />{cart}</Link>
-                <Link to='/login' className=' text-decoration-none p-1'>Loginnn</Link>
+                <Link to='/login' className=' text-decoration-none p-1'>Login</Link>
                 <Link to='/signup' className=' text-decoration-none p-1'>Sign up</Link>
             </div>
         </div>
